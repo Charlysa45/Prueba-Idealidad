7d54becc0cb9d82a605db9123fd7e324f11808ad
Carlos Rincón <carlos.rincon@uniagustiniana.edu.co>
2023-02-07 11:49:45 -0500

    token updated v2

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const baseUrl = 'https://api.spotify.com/v1/browse'
-const token = 'BQCCkgSUoYEIbuITNw9ADIBTWtTY2_W_eT0Yo4IkLDcxs_XtSvYsrbonHzNQ6I95PsnejBUCtkELqpswEgszqJt-SY8EdVfFWWAAjIQFO3uZTNjVGFJFQ54aIiRTe4DOH1LQ99S1aVy56XUNfnqIYRXDRzav7bPoEP92ZkNi0ff4fg'
+const token = 'BQDqsQiwMyZZk63FLRbL1ADr1AJGduRn2lnF3ulnB-wN1oqBIy2gNNQLZwNT6kX0lr1jMRgCIJy7-TvbgVB_EDG0wwCOoUqhMquKx7f9hoZnzJBVp_uTrQR1j6gAkt47XvnNVYTg-x21NniMx4zp9uAb8WObB7GhbZkhRF1xlwY9PQ'
 
 export const getData = async(queryParam) => {
     const config = {
